56dc742e19e3670e8d1b6367a8a60a8fe6e5a0af
Lucas Buermeyer <lbuermeyer@gmail.com>
2022-04-19 19:58:39 +0000

    4 vescs working

--- navigation/src/nodes/driveToPoint.py
+++ navigation/src/nodes/driveToPoint.py
@@ -3,19 +3,22 @@
 import rospy
 import os
 import time
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Float64
 from std_msgs.msg import Int16
-from MQP.msg import gpsCoords
+from sensor_msgs.msg import NavSatFix
+
 import math
 
 class pointDriver:
     def __init__(self):
         rospy.init_node("pointDriver")
-        rospy.Subscriber("heading", Float32, self.headingCallback, queue_size=1)
-        rospy.Subscriber("/gps", gpsCoords, self.gpsCoordsCallback, queue_size=1)
-        rospy.Subscriber("/gps/setpoint", gpsCoords, self.goalCallback, queue_size=1)
-        self.pubRight = rospy.Publisher("Right", Int16, queue_size=1)
-        self.pubLeft = rospy.Publisher("Left", Int16, queue_size=1)
+        rospy.Subscriber("/compass", Float32, self.headingCallback, queue_size=1)
+        rospy.Subscriber("/gps", NavSatFix, self.gpsCoordsCallback, queue_size=1)
+        rospy.Subscriber("/gps/setpoint", NavSatFix, self.goalCallback, queue_size=1)
+        self.pubRight1 = rospy.Publisher("/vesc_r1/commands/motor/speed", Float64, queue_size=1)
+        self.pubRight2 = rospy.Publisher("/vesc_r2/commands/motor/speed", Float64, queue_size=1)
+        self.pubLeft1  = rospy.Publisher("/vesc_l1/commands/motor/speed", Float64, queue_size=1)
+        self.pubLeft2  = rospy.Publisher("/vesc_l2/commands/motor/speed", Float64, queue_size=1)
 
         self.pth = 0.0
         self.gth = 0.0
@@ -38,28 +41,25 @@ class pointDriver:
         self.pth = msg.data
     
     def gpsCoordsCallback(self, msg):
-        self.lat = msg.lat
-        self.lon = msg.lon
+        self.lat = msg.latitude
+        self.lon = msg.longitude
 
     def goalCallback(self, msg):
-        self.goal[0] = msg.lat
-        self.goal[1] = msg.lon
+        self.goal[0] = msg.latitude
+        self.goal[1] = msg.longitude
 
     def rotate(self, angle, aspeed):
         dir = self.getBestAngle(angle)
         if dir < 0:
-            self.pubRight.publish(aspeed)
-            self.pubLeft.publish(-aspeed)
+            self.sendSpeeds(aspeed,-aspeed)
             # print("turning left")
         else:
-            self.pubRight.publish(-aspeed)
-            self.pubLeft.publish(aspeed)
+            self.sendSpeeds(-aspeed,aspeed)
             # print("turning right")
         rospy.sleep(.02)
     
     def drive(self, linSpeed):
-        self.pubRight.publish(linSpeed)
-        self.pubLeft.publish(linSpeed)
+        self.sendSpeeds(linSpeed,linSpeed)
         # print("driving straight")
         # print(self.pth)
 
@@ -67,9 +67,16 @@ class pointDriver:
         self.gth = math.degrees(math.atan2(self.goal[0] - self.lat, self.goal[1] - self.lon))-90
         # print(self.gth, self.goal, self.lat, self.lon)
     
+    def sendSpeeds(self, rSpeed, lSpeed):
+        self.pubRight1.publish(rSpeed)
+        self.pubRight2.publish(rSpeed)
+        self.pubLeft1.publish(lSpeed)
+        self.pubLeft2.publish(lSpeed)
+        print(rSpeed, lSpeed)
+    
     def run(self):
-        linspeed = 30
-        aspeed = 15
+        linspeed = 20000
+        aspeed = 10000
         while not rospy.is_shutdown():
             self.angleToGoal()
             print(self.getBestAngle(self.gth), self.goal)
